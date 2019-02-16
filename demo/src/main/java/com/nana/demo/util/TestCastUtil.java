4ed9ec1b2b71962a0248fa95d3fdcf0dbfc439e4
liuna <liuna@live.cn>
2019-02-16 09:45:59 +0800

    init

---
+++ demo/src/main/java/com/nana/demo/util/TestCastUtil.java
@@ -0,0 +1,10 @@
+package com.nana.demo.util;
+
+import com.nana.util.CastUtil;
+
+public class TestCastUtil {
+    public static void main(String[] args) {
+        int i = CastUtil.castInt(1);
+        System.out.println(i);
+    }
+}
